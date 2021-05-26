5903de3f57d9d05d676d7eccb3a6dc1b9dad9c83
carloshmachado <carlosmachadosax1@gmail.com>
2021-05-26 19:21:04 -0300

    ADD jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  coverageDirectory: ['<rootDir>/src/**/*.ts'],
-  coverageProvider: 'coverage',
-  testEnvorinment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
